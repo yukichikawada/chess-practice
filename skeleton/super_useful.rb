cefe8c63a8f5d9365ac8984ce858aca71c0a494f
yukichi <yukichikawada@gmail.com>
2018-02-19 13:56:04 -0800

    Handle friendship errors

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,6 +1,10 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue StandardError
+    nil
+  end
 end
 
 # PHASE 3
@@ -9,23 +13,32 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue
+    retry
+  end
+end
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
+    if yrs_known < 5
+      raise "Must know for at least 5 years"
+    elsif name.empty? || fav_pastime.empty?
+      raise "name and fav_pastime has to be more than 0 char"
+    end
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
   end
@@ -39,8 +52,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
